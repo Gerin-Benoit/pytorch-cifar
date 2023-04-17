4e8a79b6c5c22f01c7bd50350b52e23743658892
Gerin-Benoit <benoit.gerin@uclouvain.be>
2023-04-17 11:26:56 +0200

    bf

--- fit_gmm.py
+++ fit_gmm.py
@@ -116,6 +116,6 @@ if not os.path.isdir(args.save_path):
     os.mkdir(args.save_path)
 save_name = os.path.join(args.save_path, model_name)
 state_dict = {'mean': gaussians_model.loc, 'covariance_matrix': gaussians_model.covariance_matrix}
-print('mean', gaussians_model.shape)
-print('cov', gaussians_model.shape)
+print('mean', gaussians_model.loc.shape)
+print('cov', gaussians_model.covariance_matrix.shape)
 torch.save(state_dict, save_name)
