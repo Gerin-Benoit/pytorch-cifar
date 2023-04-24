ddd9476209a8ea1143e77d4cf3666f73e4b6595f
Benoit <benoit.gerin@uclouvain.be>
2023-04-24 17:22:34 +0200

    bf

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ parser.add_argument('--num_workers', type=int, default=12, help='Number of worke
 
 parser.add_argument('--c', type=float, default=0, help='Lipschitz constant: 0 for no SN, positive for soft, negative '
                                                        'for hard')
-parser.add_argument('--norm_layer', default='batchnorm', help='norm layer to use : batchnorm or actnorm or actnorm2')
+parser.add_argument('--norm_layer', default='batchnorm', help='norm layer to use : batchnorm or actnorm or actnorm_2d2')
 
 parser.add_argument('--mod', action='store_true', default=False, help='use increased sensitivity: average pooling shortcut and leaky relu')
 
@@ -111,7 +111,7 @@ if args.wandb_project == 'CIFAR10':
     num_classes = 10 
 elif args.wandb_project == 'CIFAR100':
     num_classes = 100
-net = ResNet50(c=args.c, num_classes=num_classes, norm_layer=args.norm_layer, device=device, mod=args.mod)
+net = ResNet50(c=args.c, num_classes=num_classes, norm_layer=args.norm_layer, device=device, mod=args.mod, fc_sn=args.fc_sn)
 net = net.to(device)
 print(net)
 """
