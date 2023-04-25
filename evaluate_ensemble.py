b73a363a0c4403760ca2ac4327ff4fff9872af3e
Gerin-Benoit <benoit.gerin@uclouvain.be>
2023-04-25 18:04:41 +0200

    test p(x|c)

--- evaluate_ensemble.py
+++ evaluate_ensemble.py
@@ -186,7 +186,7 @@ def evaluate(testloader, nets, gmms_loc=None, gmms_cov=None, domain_shift = None
                 correct_mean += predicted.eq(targets).sum().item()
 
                 for i, (loc, cov) in enumerate(zip(gmms_loc, gmms_cov)):
-                    cls = torch.argmax(outputs[i], dim=0)
+                    cls = torch.argmax(outputs[i,:,1], dim=0)
                     print(cls, cls.shape)
                     out = gmm_get_logits_given_class(loc, cov, fms[i], cls)
                     gmm = distributions.MultivariateNormal(loc=loc, covariance_matrix=cov)
