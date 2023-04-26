0393be9a5bacff4d2f0bd5bb312d773994f0e414
Gerin-Benoit <benoit.gerin@uclouvain.be>
2023-04-26 16:11:22 +0200

    test

--- evaluate_ensemble.py
+++ evaluate_ensemble.py
@@ -214,6 +214,7 @@ def evaluate(testloader, nets, gmms_loc=None, gmms_cov=None, domain_shift = None
 
                 # Compute the mean of outputs averaged by log_weights
                 weighted_average_output = sum_weighted_outputs / sum_exp_log_weights
+                print(weighted_average_output.shape)
 
                 #th_outputs = torch.where(outputs==max_outputs,1,0).float()
                 #print("-"*20)
