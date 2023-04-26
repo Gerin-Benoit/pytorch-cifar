3b7f1c2c8f3ccfeaed180f0bd49ee84bba77eff0
Gerin-Benoit <benoit.gerin@uclouvain.be>
2023-04-26 10:31:50 +0200

    more test

--- evaluate_ensemble.py
+++ evaluate_ensemble.py
@@ -202,9 +202,9 @@ def evaluate(testloader, nets, gmms_loc=None, gmms_cov=None, domain_shift = None
                 temperature = args.temp
 
                 #confidences = F.softmax(confidences / temperature, dim=0)
-                print(confidences.shape)
+                #print(confidences.shape)
                 confidences = confidences.unsqueeze(2).repeat(1,1,num_classes)
-                print(confidences.shape)
+                #print(confidences.shape)
                 weighted_average_output = torch.mean(torch.sum(confidences * outputs, dim=0) / torch.sum(confidences, dim=0, keepdim=True), dim=0)
                 #weighted_average_output = torch.sum(confidences*outputs, dim=0)#torch.mean(torch.sum(confidences*outputs, dim=0)/torch.sum(confidences, dim=0, keepdim=True), dim=0)
 
@@ -238,9 +238,9 @@ def evaluate(testloader, nets, gmms_loc=None, gmms_cov=None, domain_shift = None
         net = net.to('cpu')
         
 
-
-evaluate(testloader, nets_constrained, gmms_loc, gmms_cov)
 evaluate(testloader, nets_unconstrained)
+evaluate(testloader, nets_constrained, gmms_loc, gmms_cov)
+
 
 
 
