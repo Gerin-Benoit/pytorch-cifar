bcefdd1e8001bcdafac19638d37afa7f2b0d908a
Gerin-Benoit <benoit.gerin@uclouvain.be>
2023-05-05 13:18:39 +0200

    bf

--- models/resnet.py
+++ models/resnet.py
@@ -23,29 +23,26 @@ class ConcentrateNorm(nn.Module):
         if affine:
             self.gamma = nn.Parameter(torch.ones(num_features))
             self.beta = nn.Parameter(torch.zeros(num_features))
-        self.register_buffer('running_mean', torch.zeros(num_features))
-        self.register_buffer('running_var', torch.ones(num_features))
+        self.register_buffer('running_mean', torch.zeros(1))
+        self.register_buffer('running_var', torch.ones(1))
 
     def forward(self, x):
         B, C, H, W = x.shape
         if self.training:
-            print(x.shape)
             batch_norm = torch.linalg.norm(x.view(B, -1), dim=1)  # shape (B,)
-            print(batch_norm.shape)
-            batch_mean_norm = batch_norm.mean().expand(B)  # shape (B,)
-            print(batch_mean_norm.shape)
+            batch_mean_norm = batch_norm.mean()  # shape (B,)
             batch_var_norm = batch_norm.var(unbiased=False)
             # batch_mean = x.mean(dim=0)
             # batch_var = x.var(dim=0, unbiased=False)
             self.running_mean = self.momentum * self.running_mean + (1 - self.momentum) * batch_mean_norm
             self.running_var = self.momentum * self.running_var + (1 - self.momentum) * batch_var_norm
 
-            batch_norm_expand = batch_norm.unsqueeze(1).unsqueeze(2).unsqueeze(3).expand(1, C, H, W)
+            batch_norm_expand = batch_mean_norm.unsqueeze(1).unsqueeze(2).unsqueeze(3).expand(1, C, H, W)
         else:
             batch_norm = self.running_mean
             batch_var_norm = self.running_var
 
-            batch_norm_expand = batch_norm.expand(B, C, H, W)
+            batch_norm_expand = batch_norm.unsqueeze(1).unsqueeze(2).unsqueeze(3).expand(1, C, H, W)
 
         mask = batch_norm_expand < 1
         y = torch.zeros_like(x, device=x.device)
