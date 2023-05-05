d0bc1f72b9bdfeeb5a497fd9caf44464fabcb49e
Gerin-Benoit <benoit.gerin@uclouvain.be>
2023-05-05 11:59:57 +0200

    bf

--- models/resnet.py
+++ models/resnet.py
@@ -32,13 +32,14 @@ class ConcentrateNorm(nn.Module):
             print(x.shape)
             batch_norm = torch.linalg.norm(x.view(B, -1), dim=1)  # shape (B,)
             print(batch_norm.shape)
-            batch_mean_norm = batch_norm.mean(dim=0, keepdim=True)  # shape (1,)
+            batch_mean_norm = batch_norm.mean(dim=0, keepdim=True)  # shape (B,)
+            print(batch_mean_norm.shape)
             batch_var_norm = batch_norm.var(unbiased=False)
             # batch_mean = x.mean(dim=0)
             # batch_var = x.var(dim=0, unbiased=False)
             self.running_mean = self.momentum * self.running_mean + (1 - self.momentum) * batch_mean_norm
             self.running_var = self.momentum * self.running_var + (1 - self.momentum) * batch_var_norm
-            print(batch_mean_norm.shape)
+
             batch_norm_expand = batch_norm.unsqueeze(1).unsqueeze(2).unsqueeze(3).expand(1, C, H, W)
         else:
             batch_norm = self.running_mean
